9f40ab6854d03d115bb74ae7b8a98668df27fa3e
Mateo <57046036+prbleems@users.noreply.github.com>
2025-04-12 02:41:42 -0400

    Pagina Update
    Cambie y actualice la pagina

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
-
-
 const mobileToggle = document.querySelector('.mobile-menu-toggle');
 const navMenu = document.querySelector('.nav-menu');
-
 mobileToggle.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
